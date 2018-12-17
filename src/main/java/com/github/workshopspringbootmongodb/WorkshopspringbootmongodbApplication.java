e3374151dd94733a6667762e785453f07383c68a
Marco Teixeira <marco.teixeira4@fatec.sp.gov.br>
2018-12-17 08:48:05 -0200

    Primeiro commit - projeto criado

---
+++ src/main/java/com/github/workshopspringbootmongodb/WorkshopspringbootmongodbApplication.java
@@ -0,0 +1,14 @@
+package com.github.workshopspringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopspringbootmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopspringbootmongodbApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/github/workshopspringbootmongodb/WorkshopspringbootmongodbApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.workshopspringbootmongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkshopspringbootmongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
