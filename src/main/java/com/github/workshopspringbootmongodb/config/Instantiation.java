778158a633d2e23788d9b0aba10715e2a4eb6302
Marco Teixeira <marco.teixeira4@fatec.sp.gov.br>
2018-12-17 19:07:02 -0200

    Instanciação da base de dados

---
+++ src/main/java/com/github/workshopspringbootmongodb/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.github.workshopspringbootmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.workshopspringbootmongodb.domain.User;
+import com.github.workshopspringbootmongodb.domain.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll(); // para limpar a coleção no MongoDB e criar as novas instancias abaixo
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));//para criar uma lista e salvar no banco de dados
+	}
+
+}
