ff4b14699478542f77c50a0bb5d9d50779329151
Marco Teixeira <marco.teixeira4@fatec.sp.gov.br>
2018-12-20 10:07:17 -0200

    Consulta simples com query methods

--- src/main/java/com/github/workshopspringbootmongodb/domain/repository/PostRepository.java
+++ src/main/java/com/github/workshopspringbootmongodb/domain/repository/PostRepository.java
@@ -1,14 +1,24 @@
 package com.github.workshopspringbootmongodb.domain.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.github.workshopspringbootmongodb.domain.Post;
-import com.github.workshopspringbootmongodb.domain.User;
 
 
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/github/workshopspringbootmongodb/resources/PostResource.java
+++ src/main/java/com/github/workshopspringbootmongodb/resources/PostResource.java
@@ -1,24 +1,19 @@
 package com.github.workshopspringbootmongodb.resources;
 
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.github.workshopspringbootmongodb.domain.Post;
-import com.github.workshopspringbootmongodb.domain.User;
 import com.github.workshopspringbootmongodb.domain.service.PostService;
-import com.github.workshopspringbootmongodb.domain.service.UserService;
-import com.github.workshopspringbootmongodb.dto.UserDTO;
+import com.github.workshopspringbootmongodb.resources.util.URL;
 
 @RestController
 @RequestMapping(value="/posts")
@@ -33,6 +28,13 @@ public class PostResource {
  	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 	
